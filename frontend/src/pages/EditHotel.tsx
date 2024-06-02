c289bb7db9bd9a537576a26710a60281b2ed76ba
Rudra <rudrahimangshoo2020@gmail.com>
2024-06-03 01:16:09 +0600

    edit-hotel page

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useMutation, useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
+import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
-  return (
-    <div>EditHotel</div>
-  )
-}
+  const { hotelId } = useParams();
+  const { showToast } = useAppContext();
 
-export default EditHotel
+  const { data: hotel } = useQuery(
+    "fetchMyHotelById",
+    () => apiClient.fetchMyHotelById(hotelId || ""),
+    {
+      enabled: !!hotelId,
+    }
+  );
+
+  const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return (
+    <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isLoading} />
+  );
+};
+
+export default EditHotel;
